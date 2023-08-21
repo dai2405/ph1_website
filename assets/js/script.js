d87509d30a7aed94f2683965836f5bd0b3cae029
hatadaiki <daiki.hata.0224@icloud.com>
2023-08-21 10:22:18 +0900

    url発行

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+console.log("HEllo World!");
+console.log("こんにちは")
+console.log("I am learning JavaScript")
+console.log(5+2)
+console.log(6*2)
+console.log(12/3)
+console.log(5*7)
+console.log("2+5")
+const variable="代入する値"
+console.log(variable)
+// variable="更新しました"
+console.log(variable)
+const introduce=function(){
+    console.log("こんにちは")
+    console.log("POSSE太郎です！")
+}
+
+introduce();
+
+const calculateTotalPrice=function(price,quantity){
+    const TotalPrice=price*quantity
+    console.log(TotalPrice);
+}
+
+calculateTotalPrice(200,3);
+
+const number=15;
+
+if (number>10){
+    console.log("大きいです");
+}else{
+    console.log("小さいです")
+}
+
+const user="POSSE太郎";
+
+if(user=="POSSE太郎"){
+    console.log("正解です！！")
+}else if(user=="POSSE次郎"){
+    console.log("いまいちです！！")
+}else{
+    console.log("不正解です！！")
+}
+
+for(let number=1; number<=10; number++){
+    console.log(number);
+}
+
+for(let number=5; number<=15; number++){
+    console.log(number);
+}
+
+let about = document.getElementById('about');
+console.log(about);
+
