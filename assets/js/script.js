fe27cb27511a72598ac379dff86b6b8c0c1106bb
hatadaiki <daiki.hata.0224@icloud.com>
2023-09-10 12:31:59 +0900

    jsバグ改善

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,59 +15,59 @@ const introduce=function(){
     console.log("POSSE太郎です！")
 }
 
-introduce();
+// introduce();
 
-const calculateTotalPrice=function(price,quantity){
-    const TotalPrice=price*quantity
-    console.log(TotalPrice);
-}
+// const calculateTotalPrice=function(price,quantity){
+//     const TotalPrice=price*quantity
+//     console.log(TotalPrice);
+// }
 
-calculateTotalPrice(200,3);
+// calculateTotalPrice(200,3);
 
-const number=15;
+// const number=15;
 
-if (number>10){
-    console.log("大きいです");
-}else{
-    console.log("小さいです")
-}
+// if (number>10){
+//     console.log("大きいです");
+// }else{
+//     console.log("小さいです")
+// }
 
-const user="POSSE太郎";
+// const user="POSSE太郎";
 
-if(user=="POSSE太郎"){
-    console.log("正解です！！")
-}else if(user=="POSSE次郎"){
-    console.log("いまいちです！！")
-}else{
-    console.log("不正解です！！")
-}
+// if(user=="POSSE太郎"){
+//     console.log("正解です！！")
+// }else if(user=="POSSE次郎"){
+//     console.log("いまいちです！！")
+// }else{
+//     console.log("不正解です！！")
+// }
 
-for(let number=1; number<=10; number++){
-    console.log(number);
-}
+// for(let number=1; number<=10; number++){
+//     console.log(number);
+// }
 
-for(let number=5; number<=15; number++){
-    console.log(number);
-}
+// for(let number=5; number<=15; number++){
+//     console.log(number);
+// }
 
-let about = document.getElementById('about');
-console.log(about);
+// let about = document.getElementById('about');
+// console.log(about);
 
-let title = document.getElementsByClassName('title-en');
-console.log(title[1].innerHTML);
+// let title = document.getElementsByClassName('title-en');
+// console.log(title[1].innerHTML);
 
-title.innerHTML='About Edited';
+// title.innerHTML='About Edited';
 
-const hello = function(){
-    console.log('こんにちは');
-}
+// const hello = function(){
+//     console.log('こんにちは');
+// }
 
-let title = document.querySelector('title-en');
-text.innerHTML = 'About Edited';
+// let title = document.querySelector('title-en');
+// text.innerHTML = 'About Edited';
 
-const btn = document.getElementById('js-headerButton');
+// const btn = document.getElementById('js-headerButton');
 
-btn.addEventListener('click',hello);
+// btn.addEventListener('click',hello);
 
 const header = document.getElementById('js-header');
 const button = document.getElementById("js-headerButton");
